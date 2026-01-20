fadfc931dc0addf7e7bb5b43143b9baeeb7c65a6
Khushboo Malik <mail.khushboomalik@gmail.com>
2026-01-20 22:59:34 +0530

    Added code for document ingestion and generating vector embeddings

---
+++ src/ingest.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_and_chunk(file_path):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_documents(documents)
+    return chunks
---
+++ src/vector_store.py
@@ -0,0 +1,10 @@
+from langchain_community.vectorstores import Chroma
+
+def create_or_load_vectorstore(chunks, embedding_model):
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory="chroma"
+    )
+    vectorstore.persist()
+    return vectorstore
